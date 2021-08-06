da70146a76010ff289ced05ee9b63ddb5e82b7dc
Andrew Van Tassel <avantassel@gmail.com>
2021-08-05 19:16:50 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# See-Oh-Two
+
+![Logo](images/seeohtwo-eye-logo.png)
+
+See-Oh-Two was developed for the [Reinventing Healthy Spaces
+with Amazon Web Services challenge](https://www.hackster.io/contests/Healthy-Spaces-with-AWS) and is available on [Hackster.io](https://www.hackster.io/avantassel/see-oh-two-ea8503).
+
+
+The goal of See-Oh-Two is to see the levels around you for indoor spaces and alert you about possible dangerous levels in an effort to create healthy spaces.  Whether that space is work or home.  
+
+See-Oh-Two measures CO2, TVOCs and decibel levels.  One or many devices could be installed in a space.  The realtime data will be displayed on the M5 Core2 device screen along with colored LED lights based on current levels.  This data is also sent in realtime to AWS, which would allow people in a geo fenced area to be notified after installing an app.
+
+## CO2
+
+*Health effects of high levels of carbon dioxide*
+
+CO2 is considered to be minimally toxic by inhalation. The primary health effects caused by CO2 are the result of its behavior as a simple asphyxiant. A simple asphyxiant is a gas which reduces or displaces the normal oxygen in breathing air.
+
+Symptoms of mild CO2 exposure may include headache and drowsiness. At higher levels,rapid breathing, confusion, increased cardiac output, elevated blood pressure and increased arrhythmias may occur.
+
+Breathing oxygen depleted air caused by extreme CO2 concentrations can lead to death by
+suffocation. 
+
+Source: https://www.fsis.usda.gov/sites/default/files/media_file/2020-08/Carbon-Dioxide.pdf
+
+## TVOC
+
+*Health effects of high levels of volatile organic compounds*
+
+Symptons include eye, nose and throat irritation. Headaches, loss of coordination and nausea.  Long exposure to high levels can cause damage to liver, kidney and the central nervous system. Some organics can cause cancer in animals, some are suspected or known to cause cancer in humans.
+
+Source: https://www.epa.gov/indoor-air-quality-iaq/volatile-organic-compounds-impact-indoor-air-quality
+
+
+## Decibels
+
+*Health effects of high sound levels*
+
+| dB | Damage |
+|---|---|
+| 80 - 85 | Damage to hearing possible after 2 hours of exposure |
+| 95 | Damage to hearing possible after about 50 minutes of exposure |
+| 100 | Hearing loss possible after 15 minutes |
+| 105 - 110 | Hearing loss possible in less than 5 minutes |
+| 110 | Hearing loss possible in less than 2 minutes |
+| 120 | Pain and ear injury 
+| 140 - 150 | Pain and ear injury
+
+
+Source: https://www.cdc.gov/nceh/hearing_loss/what_noises_cause_hearing_loss.html
+
+## Components
+
+- [M5 AWS Core2](https://shop.m5stack.com/collections/stack-series/products/m5stack-core2-esp32-iot-development-kit-for-aws-iot-edukit)
+- [SGP30](https://shop.m5stack.com/products/tvoc-eco2-gas-unit-sgp30) CO2 Sensor
+- [SGP30](https://shop.m5stack.com/products/tvoc-eco2-gas-unit-sgp30) TVOC Total Volatile Organic Compounds
+- [SPM1423](https://shop.m5stack.com/products/pdm-microphone-unit-spm1423) (Built into the M5 AWS Core2) microphone used to record decibel levels is an enhanced far-field MEMS microphone.
---
+++ see-oh-two.ino
@@ -0,0 +1,272 @@
+//SGP30 needs 15 seconds to initialize calibration after power on.
+//The screen will display TVOC and CO2
+
+#include <M5Core2.h>
+#include <driver/i2s.h>
+#include "fft.h"
+#include "Adafruit_SGP30.h"
+#include "FastLED.h"
+
+#define PIN_CLK  0
+#define PIN_DATA 34
+
+#define MODE_MIC 0
+
+Adafruit_SGP30 sgp;
+int i = 15;
+long last_millis = 0;
+float decibels = 0;
+
+TFT_eSprite DisFFTbuff =  TFT_eSprite(&M5.Lcd);
+static QueueHandle_t fftvalueQueue = nullptr;
+static QueueHandle_t i2sstateQueue = nullptr;
+
+#define LEDS_PIN 25
+#define LEDS_NUM 10
+CRGB ledsBuff[LEDS_NUM];
+
+void header(const char *string, uint16_t color)
+{
+    M5.Lcd.fillScreen(color);
+    M5.Lcd.setTextSize(1);
+    M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+    M5.Lcd.fillRect(0, 0, 360, 30, TFT_BLACK);
+    M5.Lcd.setTextDatum(TC_DATUM);
+    M5.Lcd.drawString(string, 160, 3, 4); 
+}
+
+typedef struct {
+    uint8_t state;
+    void* audioPtr;
+    uint32_t audioSize;
+} i2sQueueMsg_t;
+
+bool InitI2SSpakerOrMic(int mode){
+
+    i2s_config_t i2s_config = {
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_PDM),
+        .sample_rate = 16000,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT, // is fixed at 12bit, stereo, MSB
+        .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
+        .communication_format = I2S_COMM_FORMAT_I2S,
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+        .dma_buf_count = 2,
+        .dma_buf_len = 1024,
+    };
+    if (mode == MODE_MIC)
+    {
+        i2s_config.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_PDM);
+    }
+
+    i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+    i2s_pin_config_t pin_config;
+    pin_config.bck_io_num   = I2S_PIN_NO_CHANGE;
+    pin_config.ws_io_num    = PIN_CLK;
+    pin_config.data_out_num = I2S_PIN_NO_CHANGE;
+    pin_config.data_in_num  = PIN_DATA;
+
+    i2s_set_pin(I2S_NUM_0, &pin_config);
+    i2s_set_clk(I2S_NUM_0, 44100, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_MONO);
+
+    return true;
+}
+
+static void i2sMicroFFTtask(void *arg) {
+    uint8_t FFTDataBuff[128];
+    uint8_t FFTValueBuff[24];
+    uint8_t* microRawData = (uint8_t*)calloc(2048,sizeof(uint8_t));
+    size_t bytesread;
+    int16_t* buffptr;
+    double data = 0;
+    float adc_data;
+    uint16_t ydata;
+    uint32_t subData;
+
+    uint8_t state = MODE_MIC;
+    i2sQueueMsg_t QueueMsg;
+    while(1)
+    {
+        if( xQueueReceive(i2sstateQueue,&QueueMsg,(TickType_t)0) == pdTRUE)
+        {
+            if( QueueMsg.state == MODE_MIC )
+            {
+                InitI2SSpakerOrMic(MODE_MIC);
+                state = MODE_MIC;
+            }
+        }
+        else if( state == MODE_MIC )
+        {
+            fft_config_t *real_fft_plan = fft_init(1024, FFT_REAL, FFT_FORWARD, NULL, NULL);
+            i2s_read(I2S_NUM_0, (char *)microRawData, 2048, &bytesread, (100 / portTICK_RATE_MS));
+            buffptr = (int16_t*)microRawData;            
+
+            for ( int count_n = 0; count_n < real_fft_plan->size; count_n++)
+            {
+                adc_data = (float)map(buffptr[count_n], INT16_MIN, INT16_MAX, -2000, 2000);
+                real_fft_plan->input[count_n] = adc_data;                
+            }
+            
+            // https://circuitdigest.com/microcontroller-projects/arduino-sound-level-measurement
+            // couldn't find a way to convert PDM MEMS to dB
+            // this seems to be pretty close after side by side with a decibel reader
+            decibels = adc_data / 1.6;
+            
+            fft_execute(real_fft_plan);
+
+            for ( int count_n = 1; count_n < real_fft_plan->size / 4; count_n++)
+            {
+                data = sqrt(real_fft_plan->output[2 * count_n] * real_fft_plan->output[2 * count_n] + real_fft_plan->output[2 * count_n + 1] * real_fft_plan->output[2 * count_n + 1]);
+                if ((count_n - 1) < 128)
+                {
+                    data = ( data > 2000 ) ? 2000 : data;
+                    ydata = map(data, 0, 2000, 0, 255);
+                    FFTDataBuff[128 - count_n] = ydata;
+                }
+            }
+
+            for( int count = 0; count < 24; count++ )
+            {
+                subData = 0;
+                for( int count_i = 0; count_i < 5; count_i++ )
+                {
+                    subData += FFTDataBuff[count * 5 + count_i ];
+                }
+                subData /= 5;
+                FFTValueBuff[count] = map(subData,0,255,0,8);
+            }
+            xQueueSend( fftvalueQueue, (void * )&FFTValueBuff, 0 );            
+            fft_destroy(real_fft_plan);             
+        }
+        else
+        {
+            delay(10);
+        }
+    }
+}
+
+void microPhoneSetup()
+{
+    fftvalueQueue = xQueueCreate(5, 24 * sizeof(uint8_t));
+    if( fftvalueQueue == 0 )
+    {
+        return;
+    }
+
+    i2sstateQueue = xQueueCreate(5, sizeof(i2sQueueMsg_t));
+    if( i2sstateQueue == 0 )
+    {
+        return;
+    }
+
+    InitI2SSpakerOrMic(MODE_MIC);
+    xTaskCreatePinnedToCore(i2sMicroFFTtask, "microPhoneTask", 4096, NULL, 3, NULL, 0);
+
+    DisFFTbuff.createSprite(310,160);
+}
+
+void MicroPhoneFFT()
+{
+    uint8_t FFTValueBuff[24];
+    xQueueReceive( fftvalueQueue, (void * )&FFTValueBuff, portMAX_DELAY );
+    DisFFTbuff.fillRect(0,0,320,54,DisFFTbuff.color565(0x00,0x00,0x00));
+    uint32_t colorY = DisFFTbuff.color565(0xff,0x9c,0x00);
+    uint32_t colorG = DisFFTbuff.color565(0x66,0xff,0x00);
+    uint32_t colorRect;
+    for( int x = 0; x < 24; x++ )
+    {
+        for( int y = 0; y < 9; y++ )
+        {
+            if( y < FFTValueBuff[23-x] )
+            {
+                colorRect = colorY;
+            }
+            else if( y == FFTValueBuff[23-x] )
+            {
+                colorRect = colorG;
+            }
+            else
+            {
+                continue;
+            }
+            DisFFTbuff.fillRect(x*12,54-y*6 - 5,5,5,colorRect);
+        }
+    }
+    // Convert decibels
+            // https://circuitdigest.com/microcontroller-projects/arduino-sound-level-measurement
+            // decibels = (adc_data + 83.2073) / 11.003;
+//            decibels = adc_data;
+    DisFFTbuff.pushSprite(20, 160);
+}
+
+void clearScreen(){
+  M5.Lcd.fillRect(0, 100, 320, 30, TFT_BLACK);
+  FastLED.clear();
+}
+
+void setup() {
+  M5.begin(true, false, true, true);
+  M5.Lcd.fillScreen(TFT_BLACK);
+  header("See Oh Two",TFT_BLACK);
+
+  if (! sgp.begin()){
+    Serial.println("Sensor not found :(");
+    while (1);
+  }
+  
+  M5.Lcd.drawString("CO2:", 70, 40, 4);
+  M5.Lcd.drawString("TVOC:", 60, 80, 4);
+  M5.Lcd.drawString("VOL:", 70, 130, 4);
+
+  // Setup Microphone for decibel levels
+  microPhoneSetup();
+
+  // Start Side LEDs White
+  FastLED.addLeds<SK6812, LEDS_PIN>(ledsBuff, LEDS_NUM);
+  for (int i = 0; i < LEDS_NUM; i++){
+    ledsBuff[i].setRGB(255, 255, 255);
+  }
+  FastLED.show();
+}
+
+void loop() {
+  boolean danger = false;
+  clearScreen();
+  MicroPhoneFFT();  
+
+  if (!sgp.IAQmeasure()) {
+    Serial.println("Measurement failed");
+    return;
+  }
+  
+  if (!sgp.IAQmeasure()) {
+    M5.Lcd.drawString("N/A", 140, 40 , 4);
+    M5.Lcd.drawString("N/A", 130, 80 , 4);
+  } else {
+    M5.Lcd.drawNumber(sgp.eCO2, 140, 40, 4);
+    M5.Lcd.drawNumber(sgp.TVOC, 130, 80 , 4);
+    // Danger Sidebar LED
+    danger = (sgp.eCO2 > 400);
+  }
+  M5.Lcd.drawString("ppb", 210, 80, 4);
+  M5.Lcd.drawString("ppm", 210, 40, 4);
+  
+  M5.Lcd.drawNumber(decibels, 140, 130 , 4);  
+  M5.Lcd.drawString("dB", 210, 130, 4);
+
+  if(!danger)
+    danger = (decibels > 85);
+
+  if(danger){
+    FastLED.addLeds<SK6812, LEDS_PIN>(ledsBuff, LEDS_NUM);
+    for (int i = 0; i < LEDS_NUM; i++){
+      ledsBuff[i].setRGB(255, 0, 0);
+    }
+    FastLED.show();
+  }
+  
+  Serial.print("eCO2 "); Serial.print(sgp.eCO2); Serial.print(" ppm\t");
+  Serial.print("TVOC "); Serial.print(sgp.TVOC); Serial.print(" ppb\t");
+  Serial.print("dB "); Serial.print(decibels); Serial.println(" dB\t");
+ 
+  delay(1000);
+}
